5c548ecbb6f65e3d49eb300026c5cdc8a70088fa
Robin Fränzel <github@oronth.de>
2023-01-09 09:42:02 +0100

    implement a loop for analysing the change of variance and distribution of jumps of a chart over the time

--- analysis.py
+++ analysis.py
@@ -27,6 +27,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+
 """ 
 fehlerhandling
 differenzenchart der Sprünge: Abwärts - aufwärts, 
@@ -65,9 +66,186 @@ heatmap
 correlationsmatrix
 ausgabe mit datum
 """
+
+#
+###############################################################################
+###############################################################################
+
+# parameters
+#______________________________________________________________________________
+directory    = "stocks/"  
+interval     = 1000
+
+# reading and extracting jumps
+#______________________________________________________________________________  
+data,        \
+charts,      \
+volumes      = readFile( directory )
+
+jumps        = getJumps( data )
+
+
+for index in jumps:
+    # variables for storing
+    #__________________________________________________________________________    
+    variancevector = []
+    posvarvector   = []
+    negvarvector   = []
+    
+    densityvector  = []
+    posdensvector  = []
+    negdensvector  = []  
+  
+    jumpsize = np.size( jumps[index] )
+
+    for i in range( 0, jumpsize, interval ):
+       # progres
+       #_______________________________________________________________________
+       print( "   {} % of {}-index".format( np.round( 100*i/jumpsize, 2 ), index ) )
+       
+            
+       # computing histogram of interval
+       #_______________________________________________________________________
+       histogram,   \
+       histpos,     \
+       histneg      = makeHistogram( { index : jumps[index][i:(i+1)*interval ] } )         
+       #makeHistogram accepts only dictionaries. Therefore overgive dictionary built of last N jumps
+
+
+       # computing keyvalues of interval
+       #_______________________________________________________________________      
+       histkeyvalues = determineValues( histogram )                            # keyvalues of last N jumps#
+       poskeyvalues  = determineValues( histpos )
+       negkeyvalues  = determineValues( histneg )
+       
+       
+       # storing variances of interval
+       #_______________________________________________________________________
+       variancevector.append( histkeyvalues[index]["variance"] )               # variance of last N jumps#
+       posvarvector.append(   poskeyvalues [index]["variance"] )
+       negvarvector.append(   negkeyvalues [index]["variance"] )    
+
+        
+       # computing distribution of interval
+       #_______________________________________________________________________
+       ordereddistibutions = fitDistribution( 
+                                               histogram[ index ], 
+                                               histkeyvalues[index]["minimum"],
+                                               histkeyvalues[index]["maximum"], 
+                                               binwidth = 50,
+                                               progres=False
+                                               )       
+       bestdist  = ordereddistibutions[0]    
+       
+       ordereddistibutions = fitDistribution( 
+                                               histpos[ index ], 
+                                               poskeyvalues [index]["minimum"],
+                                               poskeyvalues [index]["maximum"], 
+                                               binwidth = 50,
+                                               progres=False
+                                               )       
+       posdist  = ordereddistibutions[0]
+       
+       ordereddistibutions = fitDistribution( 
+                                               histneg[ index ], 
+                                               negkeyvalues [index]["minimum"],
+                                               negkeyvalues [index]["maximum"], 
+                                               binwidth = 50,
+                                               progres=False
+                                               )       
+       negdist  = ordereddistibutions[0]      
+       
+       
+       # computing density of distribution
+       #_______________________________________________________________________
+       pdf = makeDensity( 
+                          bestdist[0], 
+                          bestdist[1],
+                          histkeyvalues[index]["minimum"],
+                          histkeyvalues[index]["maximum"],
+                          quantiles=False
+                          )
+       posdf = makeDensity( 
+                          posdist[0], 
+                          posdist[1],
+                          poskeyvalues[index]["minimum"],
+                          poskeyvalues[index]["maximum"],
+                          quantiles=False
+                          )
+       
+       negdf = makeDensity( 
+                          negdist[0], 
+                          negdist[1],
+                          negkeyvalues[index]["minimum"],
+                          negkeyvalues[index]["maximum"],
+                          quantiles=False
+                          )
+
+
+       # storing density
+       #_______________________________________________________________________
+       densityvector.append( pdf   )
+       posdensvector.append( posdf )
+       negdensvector.append( negdf )
+       
+
+
+       
+
+    # plotting variances
+    #__________________________________________________________________________   
+    variancevector = np.array( variancevector )
+    posvarvector   = np.array( posvarvector )
+    negvarvector   = np.array( negvarvector )    
+    
+    x = np.arange( 0, jumpsize, interval )
+    
+    y = range( 0, np.size( volumes["dax40"] ) )
+
+    plt.figure()
+    plt.plot( x, variancevector )
+    plt.plot( x, posvarvector )
+    plt.plot( x, negvarvector )
+    
+    plt.plot( y, charts["dax40"] )
+    
+    plt.title( "variances of {} in intervals of size {}".format( index, interval ) )
+    plt.show()
+    
  
+    # plotting densities
+    #__________________________________________________________________________   
+    plt.figure()
+    for pdf in densityvector:
 
+        pdf.plot( 
+                  linewidth       = 2, 
+                  #label           = distname, 
+                  #legend          = True, 
+                  antialiased     = True,
+                  color           = 'black'
+                  )
+    plt.show()
+    
+    plt.figure()
+    for pdf in posdensvector:
+        pdf.plot( 
+                  linewidth       = 2, 
+                  #label           = distname, 
+                  #legend          = True, 
+                  antialiased     = True,
+                  color           = 'black'
+                  )
+    plt.show() 
 
-
-
+    plt.figure()
+    for pdf in negdensvector:
+        pdf.plot( 
+                  linewidth       = 2, 
+                  #label           = distname, 
+                  #legend          = True, 
+                  antialiased     = True,
+                  color           = 'black'
+                  )
+    plt.show()
 
